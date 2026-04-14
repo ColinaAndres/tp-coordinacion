1e6e40144a38e071daac435d06b285a3cdef6b11
Andres Colina <acolina@fi.uba.ar>
2026-04-14 14:54:07 -0300

    add: Extraccion de logica de sumado a una struct Accumulator

---
+++ golang/src/common/accumulator/accumulator.go
@@ -0,0 +1,48 @@
+package accumulator
+
+import (
+	"github.com/7574-sistemas-distribuidos/tp-coordinacion/common/fruititem"
+)
+
+type Accumulator struct {
+	clientMaps map[string]map[string]fruititem.FruitItem
+}
+
+func NewAccumulator() *Accumulator {
+	return &Accumulator{
+		clientMaps: map[string]map[string]fruititem.FruitItem{},
+	}
+}
+
+func (accumulator *Accumulator) AddFruitItems(clientId string, fruitRecords []fruititem.FruitItem) error {
+	if _, ok := accumulator.clientMaps[clientId]; !ok {
+		accumulator.clientMaps[clientId] = map[string]fruititem.FruitItem{}
+	}
+
+	clientMap := accumulator.clientMaps[clientId]
+
+	for _, fruitRecord := range fruitRecords {
+		_, ok := clientMap[fruitRecord.Fruit]
+		if ok {
+			clientMap[fruitRecord.Fruit] = clientMap[fruitRecord.Fruit].Sum(fruitRecord)
+		} else {
+			clientMap[fruitRecord.Fruit] = fruitRecord
+		}
+	}
+	return nil
+}
+
+func (accumulator *Accumulator) RemoveClientFruitItems(clientId string) ([]fruititem.FruitItem, bool) {
+	clientMap, ok := accumulator.clientMaps[clientId]
+	if !ok {
+		return nil, false
+	}
+
+	fruitItems := make([]fruititem.FruitItem, 0, len(clientMap))
+	for _, item := range clientMap {
+		fruitItems = append(fruitItems, item)
+	}
+
+	delete(accumulator.clientMaps, clientId)
+	return fruitItems, true
+}
