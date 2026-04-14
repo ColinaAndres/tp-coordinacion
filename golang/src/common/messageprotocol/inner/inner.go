1f86cf42e24dbfa52ef6ef511651261d0163c31e
Andres Colina <acolina@fi.uba.ar>
2026-04-14 13:10:12 -0300

    refactor: protocolo interno cambia para soportar innerMessage
    Se pide que cualquier mensaje a enviar cumpla la entructura
    de innerMessage y a su vez se devuelve unicamente ese tipo de mensajes

--- golang/src/common/messageprotocol/inner/inner.go
+++ golang/src/common/messageprotocol/inner/inner.go
@@ -2,69 +2,32 @@ package inner
 
 import (
 	"encoding/json"
-	"errors"
 
-	"github.com/7574-sistemas-distribuidos/tp-coordinacion/common/fruititem"
 	"github.com/7574-sistemas-distribuidos/tp-coordinacion/common/middleware"
 )
 
-func serializeJson(message []interface{}) ([]byte, error) {
-	return json.Marshal(message)
+func serializeJson(data any) ([]byte, error) {
+	return json.Marshal(data)
 }
 
-func deserializeJson(message []byte) ([]interface{}, error) {
-	var data []interface{}
-	if err := json.Unmarshal(message, &data); err != nil {
-		return nil, err
-	}
-	return data, nil
+func deserializeJson(data []byte, value any) error {
+	return json.Unmarshal(data, value)
 }
 
-func SerializeMessage(fruitRecords []fruititem.FruitItem) (*middleware.Message, error) {
-	data := []interface{}{}
-	for _, fruitRecord := range fruitRecords {
-		datum := []interface{}{
-			fruitRecord.Fruit,
-			fruitRecord.Amount,
-		}
-		data = append(data, datum)
-	}
-
-	body, err := serializeJson(data)
+func SerializeMessage(record InnerMessage) (*middleware.Message, error) {
+	body, err := serializeJson(record)
 	if err != nil {
 		return nil, err
 	}
 	message := middleware.Message{Body: string(body)}
-
 	return &message, nil
 }
 
-func DeserializeMessage(message *middleware.Message) ([]fruititem.FruitItem, bool, error) {
-	data, err := deserializeJson([]byte((*message).Body))
+func DeserializeMessage(message *middleware.Message) (*InnerMessage, error) {
+	var innerMessage InnerMessage
+	err := deserializeJson([]byte(message.Body), &innerMessage)
 	if err != nil {
-		return nil, false, err
+		return nil, err
 	}
-
-	fruitRecords := []fruititem.FruitItem{}
-	for _, datum := range data {
-		fruitPair, ok := datum.([]interface{})
-		if !ok {
-			return nil, false, errors.New("Datum is not an array")
-		}
-
-		fruit, ok := fruitPair[0].(string)
-		if !ok {
-			return nil, false, errors.New("Datum is not a (fruit, amount) pair")
-		}
-
-		fruitAmount, ok := fruitPair[1].(float64)
-		if !ok {
-			return nil, false, errors.New("Datum is not a (fruit, amount) pair")
-		}
-
-		fruitRecord := fruititem.FruitItem{Fruit: fruit, Amount: uint32(fruitAmount)}
-		fruitRecords = append(fruitRecords, fruitRecord)
-	}
-
-	return fruitRecords, len(fruitRecords) == 0, nil
+	return &innerMessage, nil
 }
