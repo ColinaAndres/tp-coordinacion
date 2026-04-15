1ce820b40479044901523c6a7893d98cede14e8c
Andres Colina <acolina@fi.uba.ar>
2026-04-15 14:37:19 -0300

    add: Accumulator maneja estado de clientes que ya esten listos
    Este registro es util para tener en memoria la informacion y eventualmente
    borrarla

--- golang/src/common/accumulator/accumulator.go
+++ golang/src/common/accumulator/accumulator.go
@@ -7,23 +7,30 @@ import (
 )
 
 type Accumulator struct {
-	clientMaps map[string]map[string]fruititem.FruitItem
-	mutex      sync.RWMutex
+	clientMaps  map[string]map[string]fruititem.FruitItem
+	doneClients map[string]bool
+	mutex       sync.RWMutex
 }
 
 func NewAccumulator() *Accumulator {
 	return &Accumulator{
-		clientMaps: map[string]map[string]fruititem.FruitItem{},
-		mutex:      sync.RWMutex{},
+		clientMaps:  map[string]map[string]fruititem.FruitItem{},
+		doneClients: map[string]bool{},
+		mutex:       sync.RWMutex{},
 	}
 }
 
 // Agrega los registros de un cliente al acumulador,
-// sumando los registros de frutas repetidos, si no existia la entrada la crea.
+// sumando los registros de frutas repetidos, si no existia la entrada la crea pero si
+// el cliente ya habia enviado un EOF, no se agregan los registros y se devuelve false.
 // Metodo thread safe
-func (accumulator *Accumulator) AddFruitItems(clientId string, fruitRecords []fruititem.FruitItem) error {
+func (accumulator *Accumulator) AddFruitItems(clientId string, fruitRecords []fruititem.FruitItem) bool {
 	accumulator.mutex.Lock()
 	defer accumulator.mutex.Unlock()
+
+	if accumulator.doneClients[clientId] {
+		return false
+	}
 
 	if _, ok := accumulator.clientMaps[clientId]; !ok {
 		accumulator.clientMaps[clientId] = map[string]fruititem.FruitItem{}
@@ -39,7 +46,7 @@ func (accumulator *Accumulator) AddFruitItems(clientId string, fruitRecords []fr
 			clientMap[fruitRecord.Fruit] = fruitRecord
 		}
 	}
-	return nil
+	return true
 }
 
 // Elimina los registros de un cliente y devuelve la lista de items eliminados.
@@ -60,6 +67,8 @@ func (accumulator *Accumulator) RemoveClientFruitItems(clientId string) ([]fruit
 	}
 
 	delete(accumulator.clientMaps, clientId)
+	accumulator.doneClients[clientId] = true
+
 	return fruitItems, true
 }
 
@@ -81,4 +90,16 @@ func (accumulator *Accumulator) GetClientFruitItems(clientId string) ([]fruitite
 	}
 
 	return fruitItems, true
+}
+
+// Limpia la lista de clientes que ya enviaron EOF, se puede usar para liberar memoria
+// si se sabe que no van a volver a enviar registros
+// Metodo thread safe
+func (accumulator *Accumulator) CleanDoneClients() {
+	accumulator.mutex.Lock()
+	defer accumulator.mutex.Unlock()
+
+	for clientId := range accumulator.doneClients {
+		delete(accumulator.doneClients, clientId)
+	}
 }
