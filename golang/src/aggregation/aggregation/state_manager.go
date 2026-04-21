ab7a73394080433d05fe35af66b4329722890077
Andres Colina <acolina@fi.uba.ar>
2026-04-21 12:14:19 -0300

    add: StateManager incluido en agregacion para evitar rc y encapsualr logica de conteo

---
+++ golang/src/aggregation/aggregation/state_manager.go
@@ -0,0 +1,71 @@
+package aggregation
+
+import (
+	"sync"
+)
+
+type QueryState struct {
+	ReceivedCount int
+	TargetCount   int
+	EOFcount      int
+}
+
+type StateManager struct {
+	clientStates   map[string]*QueryState
+	TargetEOFCount int
+	mu             sync.RWMutex
+}
+
+func NewStateManager(targetEOFCount int) *StateManager {
+	return &StateManager{
+		clientStates:   make(map[string]*QueryState),
+		TargetEOFCount: targetEOFCount,
+	}
+}
+
+func (manager *StateManager) AddCount(clientId string, count int) {
+	manager.mu.Lock()
+	defer manager.mu.Unlock()
+
+	state, ok := manager.clientStates[clientId]
+	if !ok {
+		state = &QueryState{}
+		manager.clientStates[clientId] = state
+	}
+	state.ReceivedCount += count
+}
+
+func (manager *StateManager) MarkEOF(clientId string, total_count int) {
+	manager.mu.Lock()
+	defer manager.mu.Unlock()
+
+	state, ok := manager.clientStates[clientId]
+	if !ok {
+		state = &QueryState{}
+		manager.clientStates[clientId] = state
+	}
+	state.EOFcount++
+	state.TargetCount = total_count
+}
+
+func (manager *StateManager) DoneWaiting(clientId string) bool {
+	manager.mu.RLock()
+	defer manager.mu.RUnlock()
+
+	state, ok := manager.clientStates[clientId]
+	if !ok {
+		return false
+	}
+	return state.EOFcount == manager.TargetEOFCount
+}
+
+func (manager *StateManager) DoneReceiving(clientId string) bool {
+	manager.mu.RLock()
+	defer manager.mu.RUnlock()
+
+	state, ok := manager.clientStates[clientId]
+	if !ok {
+		return false
+	}
+	return state.ReceivedCount == state.TargetCount
+}
